c307c87ffd32ca5079702a11fc394c6199d9ba2c
raphzam <60793109+raphzam@users.noreply.github.com>
2021-07-19 17:23:32 -0400

    Refactor findFirstUniqueCharacter method

--- src/hashtables/Main.java
+++ src/hashtables/Main.java
@@ -19,22 +19,17 @@ public class Main {
 
     public static char findFirstUniqueCharacter(String str) {
         Map<Character, Integer> map = new HashMap<>();
-
-        for (char ch : str.toCharArray()) {
-            if (map.containsKey(ch)) {
-                int count = map.get(ch);
-                map.put(ch, count + 1);
-                continue;
-            }
-            map.put(ch, 1);
+        char[] chars = str.toCharArray();
+        for (char ch : chars) {
+            int count = map.containsKey(ch) ? map.get(ch) : 0;
+            map.put(ch, count + 1);
         }
 
-        for (char ch : str.toCharArray()) {
+        for (char ch : chars) {
             if (map.get(ch) == 1)
                 return ch;
         }
 
         return Character.MIN_VALUE;
-
     }
 }
