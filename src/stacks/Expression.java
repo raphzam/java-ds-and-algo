e90176bcf4e3e5de814138f29242a13b84ea98ff
raphzam <60793109+raphzam@users.noreply.github.com>
2021-07-29 12:53:23 -0400

    Add first implementation for valid parenthesis method

---
+++ src/stacks/Expression.java
@@ -0,0 +1,20 @@
+package stacks;
+
+import java.util.Stack;
+
+public class Expression {
+    public boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            }
+
+            if (ch == ')') {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
