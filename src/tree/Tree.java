67f287175a8b9134e1ae95f0198686a9e5c737a2
raphzam <60793109+raphzam@users.noreply.github.com>
2021-09-06 14:51:27 -0400

    Add validate Binary Search Tree method

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -173,4 +173,17 @@ public class Tree {
         }
     }
 
+    public boolean isBST() {
+        return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean isBST(Node root, int min, int max) {
+        if (root == null) return true;
+
+        if (root.value < min || root.value > max) return false;
+
+        return isBST(root.leftChild, min, root.value - 1) &&
+                isBST(root.rightChild, root.value + 1, max);
+    }
+
 }
